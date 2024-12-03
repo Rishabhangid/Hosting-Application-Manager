47541b2c30d42699da5ae3531600a99d5d705cf7
Rishabhangid <riishabhjangid@gmail.com>
2024-12-03 10:19:58 +0530

    Fix Cors Policy  Commit

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // IMPORTING MODULES
 const express = require("express");
-const app =express();
+const app = express();
 const cors = require("cors"); // deals with cors policy
 
 const dotenv = require("dotenv"); // stores confidential information 
-dotenv.config({path:"./config.env"}); // importing .env file
+dotenv.config({ path: "./config.env" }); // importing .env file
 
 require("./dbconnection/connection"); // coonection to database
-app.use(cors()); // cors policy
+// app.use(cors()); // cors policy
+const corsOptions = {
+    origin: 'https://yourfrontenddomain.com',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json()); // convert response into json format
 
 
 app.use(require("./routes/route")); // importing routes
 
 const PORT = process.env.PORT || 6053;
-app.listen(PORT, ()=>{console.log(`Server started at port no: ${PORT}`)}) // starting server
+app.listen(PORT, () => { console.log(`Server started at port no: ${PORT}`) }) // starting server
